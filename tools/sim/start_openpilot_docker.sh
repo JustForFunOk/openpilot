585c039faaabaef5493fb72aa6a9115cb2389ba2
Jian Sun <slgsunjian@gmail.com>
2021-11-28 16:08:02 +0800

    [sim/carla] improve carla and op sim script

--- tools/sim/start_openpilot_docker.sh
+++ tools/sim/start_openpilot_docker.sh
@@ -6,8 +6,11 @@ cd $DIR
 # expose X to the container
 xhost +local:root
 
-docker pull ghcr.io/commaai/openpilot-sim:latest
+# 'docker pull' cost too much time, and we don't need latest version every time
+# docker pull ghcr.io/commaai/openpilot-sim:latest
 
+# execute openpilot built in host machine , not in docker container
+export MOUNT_OPENPILOT=1
 OPENPILOT_DIR="/openpilot"
 if ! [[ -z "$MOUNT_OPENPILOT" ]]
 then
@@ -15,8 +18,13 @@ then
   EXTRA_ARGS="-v $OPENPILOT_DIR:$OPENPILOT_DIR -e PYTHONPATH=$OPENPILOT_DIR:$PYTHONPATH"
 fi
 
+CONTAINER_NAME="openpilot_client"
+
+# open an independent terminal for ./bridge.py
+gnome-terminal --title="bridge" \
+  -- /bin/bash -c " \
 docker run --net=host\
-  --name openpilot_client \
+  --name $CONTAINER_NAME \
   --rm \
   -it \
   --gpus all \
@@ -29,4 +37,10 @@ docker run --net=host\
   -w "$OPENPILOT_DIR/tools/sim" \
   $EXTRA_ARGS \
   ghcr.io/commaai/openpilot-sim:latest \
-  /bin/bash -c "./tmux_script.sh $*"
+  /bin/bash -c "./bridge.py $*" \
+  "
+
+sleep 0.1 # sleep 0.1 second, wait for container is running
+
+docker exec -it $CONTAINER_NAME \
+  /bin/bash -c "./launch_openpilot.sh"
