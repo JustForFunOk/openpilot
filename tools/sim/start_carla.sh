f276d680fbcc52928e2f5448b9af383366908b41
Jian Sun <slgsunjian@gmail.com>
2021-12-05 22:47:23 +0800

    add some annotation for start_carla script

--- tools/sim/start_carla.sh
+++ tools/sim/start_carla.sh
@@ -17,6 +17,13 @@ fi
 
 docker pull carlasim/carla:0.9.12
 
+# 启动Carla
+# -opengl carla0.9.12开始就不支持opengl了，这个为啥没去掉？
+# -nosound
+# -RenderOffScreen 除了不显示画面，均在正常工作
+# -benchmark 保留所有的Frame，不跳过
+# -fps=20
+# -quality-level=High 渲染的质量等级，有Low，Epic，降低质量以减轻GPU负担
 docker run \
   --rm \
   --gpus all \
